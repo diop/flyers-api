323b71557598db4a94caa3a3a91ebaad76d09a69
Patrick <porchewellness@gmail.com>
2018-01-14 09:59:21 -0800

    production env

--- src/database/config.js
+++ src/database/config.js
@@ -1,10 +1,16 @@
 const pgp = require('pg-promise')()
+require('dotenv').config();
 
 const connection = {
   host: 'localhost',
   database: 'flyers'
 }
 
+if (process.env.NODE_ENV === 'production') {
+  connection = {
+    host: process.env.DATABASE_URL
+  }
+}
 const db = pgp(connection)
 
 module.exports =  { db }
