bed54c3562fdf2b8580d263d18e99c94306638f9
Patrick <porchewellness@gmail.com>
2018-01-14 10:25:34 -0800

    adds user and password

--- src/database/config.js
+++ src/database/config.js
@@ -7,7 +7,9 @@ const pgp = require('pg-promise')()
 
 const connection = {
   host: "ec2-107-21-95-70.compute-1.amazonaws.com",
-  database: "da3ar2fu63mckn"
+  database: "da3ar2fu63mckn",
+  user: "gjaovdqqewtguy",
+  password: "2649ef6883f757f61e4ccd688189cebe12fbf020626661ef531b1953b44cd2e2"
 };
 
 const db = pgp(connection)
