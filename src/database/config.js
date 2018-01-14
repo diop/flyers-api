ea16438c5cf2b3c8d13c430000fcd752e671c886
jason00111 <emberleyjason@gmail.com>
2018-01-14 10:46:13 -0800

    heroku

--- src/database/config.js
+++ src/database/config.js
@@ -1,17 +1,19 @@
 const pgp = require('pg-promise')()
 
-// const connection = {
-//   host: 'localhost',
-//   database: 'flyers'
-// }
-
 const connection = {
-  host: "ec2-107-21-95-70.compute-1.amazonaws.com",
-  database: "da3ar2fu63mckn",
-  user: "gjaovdqqewtguy",
-  password: "2649ef6883f757f61e4ccd688189cebe12fbf020626661ef531b1953b44cd2e2"
-};
+  host: 'localhost',
+  database: 'flyers'
+}
 
-const db = pgp(connection)
+// const connection = {
+//   host: "ec2-107-21-95-70.compute-1.amazonaws.com",
+//   database: "da3ar2fu63mckn",
+//   user: "gjaovdqqewtguy",
+//   password: "2649ef6883f757f61e4ccd688189cebe12fbf020626661ef531b1953b44cd2e2"
+// };
+
+
+
+const db = pgp(process.env.DATABASE_URL || connection)
 
 module.exports =  { db }
