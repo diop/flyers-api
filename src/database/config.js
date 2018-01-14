713d8e5a2f9fa86b9b8e85b3a0a7e310e7d5a53f
Patrick <porchewellness@gmail.com>
2018-01-14 10:22:02 -0800

    adds production database

--- src/database/config.js
+++ src/database/config.js
@@ -1,9 +1,14 @@
 const pgp = require('pg-promise')()
 
+// const connection = {
+//   host: 'localhost',
+//   database: 'flyers'
+// }
+
 const connection = {
-  host: 'localhost',
-  database: 'flyers'
-}
+  host: "ec2-107-21-95-70.compute-1.amazonaws.com",
+  database: "da3ar2fu63mckn"
+};
 
 const db = pgp(connection)
 
